b63be8313ac385a0323b3a70ea007cb572c5fbd8
Tabajara951 <krib951@gmail.com>
2025-05-20 19:32:41 -0300

    primeiro commit

---
+++ Exercício01.py
@@ -0,0 +1,4 @@
+numero = int(input ('PRIMEIRO NUMERO '))
+numeroo = int(input ('SEGUNDO NUMERO CORNO! '))
+resultado = sum([numero, numeroo])
+print(f"o resultado da soma é {resultado}") 
---
+++ Exercício03.py
@@ -0,0 +1,7 @@
+a = input ('digite algo aqui ') 
+print('o tipo primitivo desse valor é ', type(a))       
+print ('só tem espaços? ', a.isspace())
+print ('só tem letras?', a.isalpha())
+print ("só tem numeros?", a.isnumeric())
+print ('é uma mistura de letras e numeros???', a.isalnum())
+
